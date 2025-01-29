51299a9e6d1d8c84c0249cfaa668cba9b83cf563
Baxrom <bahromislomov324@gmail.com>
2025-01-29 10:00:02 +0500

    dytfuyg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {  Routes, Route } from "react-router-dom";
-import Header from "./components/header";
+import Header from "./components/Header";
 import ProductPage from "./components/ProductPage";
 import All from "./components/All";
 
